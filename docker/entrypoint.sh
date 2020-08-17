9eddb70f571d9ffcc476b0653f04a308ae0ca015
flaviusburca <flavius.burca@noreply.com>
2020-08-17 19:29:17 +0300

    S3 ingestion

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "Bootstrapping BigConnect Discovery...."
 
-sed -i 's@HDFS_ROOT@'"$HDFS_ROOT"'@g' /opt/bdl/etc/hadoop/core-site.xml
+sed -i 's@TENANT_ID@'"$TENANT_ID"'@g' /opt/bdl/etc/hadoop/core-site.xml
+
+nohup $JDK8_HOME/bin/java -jar /opt/bdl/lib/spark/discovery-prep-spark-engine-4.1.0.jar > /opt/bdl/log/sparkengine.log &
 
 /opt/bdl/sbin/start-discovery.sh
