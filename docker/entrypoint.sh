8066bd5c44e2e0cbc1a2400460ba9513c9a1e513
dan.chirila <dan.chirila@m-ware.eu>
2020-08-21 17:22:38 +0300

    Fixes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,6 @@ echo "Bootstrapping BigConnect Discovery...."
 
 sed -i 's@TENANT_ID@'"$TENANT_ID"'@g' /opt/bdl/etc/hadoop/core-site.xml
 
-nohup $JDK8_HOME/bin/java -jar /opt/bdl/lib/spark/discovery-prep-spark-engine-4.1.0.jar > /opt/bdl/log/sparkengine.log &
+nohup $JDK8_HOME/bin/java -jar -Xmx16g /opt/bdl/lib/spark/discovery-prep-spark-engine-4.1.0.jar > /opt/bdl/log/sparkengine.log &
 
 /opt/bdl/sbin/start-discovery.sh
