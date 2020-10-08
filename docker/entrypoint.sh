131fba8fcf97f9372fe313e9762afd4a6610acae
i1befree <i1befree@gmail.com>
2020-10-08 11:01:30 +0900

    Fix for latest cluster changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,8 @@
 echo "Bootstrapping BigConnect Discovery...."
 
 sed -i 's@TENANT_ID@'"$TENANT_ID"'@g' /opt/bdl/etc/hadoop/core-site.xml
+sed -i 's@AWS_SECRET_KEY@'"$AWS_SECRET_KEY"'@g' /opt/bdl/etc/hadoop/core-site.xml
+sed -i 's@AWS_SECRET_KEY@'"$AWS_SECRET_KEY"'@g' /opt/bdl/etc/hadoop/core-site.xml
 
 nohup $JDK8_HOME/bin/java -jar -Xmx16g /opt/bdl/lib/spark/discovery-prep-spark-engine-4.1.0.jar > /opt/bdl/log/sparkengine.log &
 
