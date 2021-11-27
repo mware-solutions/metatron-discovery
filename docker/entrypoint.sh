4c5525be1c75fd1b18b310611b57574d830071c7
flaviusburca <flavius.burca@localhost>
2021-11-27 05:40:54 +0200

    Fix for S3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Bootstrapping BigConnect Discovery...."
 
 sed -i 's@TENANT_ID@'"$TENANT_ID"'@g' /opt/bdl/etc/hadoop/core-site.xml
-sed -i 's@AWS_SECRET_KEY@'"$AWS_SECRET_KEY"'@g' /opt/bdl/etc/hadoop/core-site.xml
+sed -i 's@AWS_ACCESS_KEY@'"$AWS_ACCESS_KEY"'@g' /opt/bdl/etc/hadoop/core-site.xml
 sed -i 's@AWS_SECRET_KEY@'"$AWS_SECRET_KEY"'@g' /opt/bdl/etc/hadoop/core-site.xml
 
 nohup $JDK8_HOME/bin/java -jar -Xmx4g -Djava.security.egd=file:/dev/./urandom /opt/bdl/lib/spark/discovery-prep-spark-engine-4.1.0.jar > /opt/bdl/log/sparkengine.log &
